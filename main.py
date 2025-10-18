76942cf1d7b7db8f7d1c29f00384c6167990848d
Eduardo Lopes Almeida <eduardoscota08@gmail.com>
2025-10-18 18:00:32 -0300

    Corrige Problema na Exibição das Mensagens Chat

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
+import time
+import random
 import streamlit as st
 from assistant_bot import assistant_bot
-import random
-from time import sleep 
+import google.generativeai as genai
 
-def start_assistant(history):
-    bot_ajuda = assistant_bot()
-    bot_ajuda.conect_client()
-    bot_ajuda.start_chat(history)
+st.set_page_config(
+    page_title='Assistente Saúde Diagnóstico',
+    page_icon=':stethoscope:'
+)
 
-    return bot_ajuda
+st.title('Sistema De Diagnóstico Inicial 🩺')
+st.caption('Chatbot feito com Gemini')
 
-def app():
-    st.header("Sistema de Diagnóstico Inicial", divider=True)
+if 'historico' not in st.session_state:
+    st.session_state.historico = []
 
-    if "historico" not in st.session_state:
-        st.session_state.historico = []
+bot = assistant_bot()
+bot.create_model()
+bot.start_chat(history=st.session_state.historico)
 
-    bot = start_assistant(st.session_state.historico)
+for mensagem in bot.chat.history:
+    if mensagem.role == 'model':
+        role = 'assistant'
+    else:
+        role = mensagem.role
+    with st.chat_message(role):
+        st.markdown(mensagem.parts[0].text)
 
-    for mensagem in bot.chat.get_history():
-        if mensagem.role == "model":
-            role = "assistant"
-        else:
-            role = mensagem.role
-        with st.chat_message(role):
-            st.markdown(mensagem.parts[0].text)
-
-    prompt = st.chat_input("Escreva sua duvida")
-    if prompt:
-        with st.chat_message('user'):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            mensagem_placeholder = st.empty()
-            mensagem_placeholder.markdown("pensando")
-            try:
-                resposta = ''
-                for chunk in bot.chat.send_message_stream(prompt):
-                    contagem_palavras = 0
-                    n_aleatorio = random.randint(5, 10)
-                    for palavra in chunk.text:
-                        resposta += palavra
-                        contagem_palavras += 1
-                        if contagem_palavras == n_aleatorio:
-                            sleep(0.05)
-                            mensagem_placeholder.markdown(resposta + '_')
-                            contagem_palavras = 0
-                            n_aleatorio = random.randint(5, 10)
-                mensagem_placeholder.markdown(resposta)
-            except Exception as e:
-                print(f"Erro na geração da resposta: {e}")
-
-app()
+prompt = st.chat_input('')
+if prompt:
+    prompt = prompt.replace('\n', ' \n')
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    with st.chat_message('assistant'):
+        mensagem_placeholder = st.empty()
+        mensagem_placeholder.markdown('Pensando...')
+        try:
+            resposta = ''
+            for chunk in bot.chat.send_message(prompt, stream=True):
+                contagem_palavras = 0
+                n_aleatorio = random.randint(5, 10)
+                for palavra in chunk.text:
+                    resposta += palavra
+                    contagem_palavras += 1
+                    if contagem_palavras == n_aleatorio:
+                        time.sleep(0.05)
+                        mensagem_placeholder.markdown(resposta + '_')
+                        contagem_palavras = 0
+                        n_aleatorio = random.randint(5, 10)
+            mensagem_placeholder.markdown(resposta)
+        except genai.types.generation_types.BlockedPromptException as e:
+            st.exception(e)
+        except Exception as e:
+            st.exception(e)
+        st.session_state.historico = bot.chat.history
